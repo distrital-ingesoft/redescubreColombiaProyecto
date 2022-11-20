7a673942fa20a5fb06208ba36029ba7477f10820
Jaiver96 <jesalazaro@correo.udistrital.edu.co>
2022-11-20 16:56:16 -0500

    se añadio la clase amigo

---
+++ src/main/java/redescubrecolombia/modelo/Amigo.java
@@ -0,0 +1,68 @@
+package redescubrecolombia.modelo;
+
+
+public class Amigo {
+    private String nombreAmigo;
+    
+    private void setNombreAmigo(String value) {
+       this.nombreAmigo = value;
+    }
+    
+    private String getNombreAmigo() {
+       return this.nombreAmigo;
+    }
+    
+    private String apellidoAmigo;
+    
+    private void setApellidoAmigo(String value) {
+       this.apellidoAmigo = value;
+    }
+    
+    private String getApellidoAmigo() {
+       return this.apellidoAmigo;
+    }
+    
+    private String emailAmigo;
+    
+    private void setEmailAmigo(String value) {
+       this.emailAmigo = value;
+    }
+    
+    private String getEmailAmigo() {
+       return this.emailAmigo;
+    }
+    
+    /**
+     * <pre>
+     *           0..*     1..1
+     * Amigo ------------------------- Usuario
+     *           amigo        &lt;       usuario
+     * </pre>
+     */
+    private Usuario usuario;
+    
+    public void setUsuario(Usuario value) {
+       this.usuario = value;
+    }
+    
+    public Usuario getUsuario() {
+       return this.usuario;
+    }
+    
+    public String getEmailAmigo(String emailAmigo) {
+       // TODO implement this operation
+       throw new UnsupportedOperationException("not implemented");
+    }
+    
+    public String getnomAm() {
+       // TODO implement this operation
+       throw new UnsupportedOperationException("not implemented");
+    }
+    
+    public String getApAm() {
+       // TODO implement this operation
+       throw new UnsupportedOperationException("not implemented");
+    }
+    
+    }
+ 
